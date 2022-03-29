f24edc8623e93c327da9fd72b65e5754831cf3ae
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-03-29 09:47:21 +0100

    my fith upload

--- config/config.cjs
+++ config/config.cjs
@@ -17,10 +17,14 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
+    client: 'pg',
     dialect: 'postgres',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 };
