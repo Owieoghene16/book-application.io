1507448346faf4417b2bb66e6df534fb4e75bd82
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-03-29 13:54:04 +0100

    my seventh commit

--- dbConfig.js
+++ dbConfig.js
@@ -3,4 +3,4 @@ import knex from 'knex';
 import knexConfig from './knexfile';
 
 const environment = process.env.DB_ENV || 'production';
-module.exports = knex(knexConfig[environment]);
+export default knex(knexConfig[environment]);
