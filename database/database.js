4a8682e4e64471328f434434b3d0a7c5cf4f374c
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-03-30 15:46:56 +0100

    my twenty-one commit

--- database/database.js
+++ database/database.js
@@ -6,7 +6,7 @@ import Borrow from '../models/borrow';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('DATABASE_URL', {
   host: 'localhost',
   dialect: 'postgres',
   operatorsAliases: false,
