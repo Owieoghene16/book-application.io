11b71f63b7c210403240b41e6cbfc662f9b7d570
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-03-30 14:06:45 +0100

    my eleventh commit

--- config/config.cjs
+++ config/config.cjs
@@ -20,8 +20,9 @@ module.exports = {
   production: {
     use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: true,
-    },
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: '127.0.0.1',
   },
 };
