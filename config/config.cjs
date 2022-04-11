06360075cef49cf9e7699f7264fba5e797881543
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-04-11 16:59:22 +0100

    running migration on circleci

--- config/config.cjs
+++ config/config.cjs
@@ -3,11 +3,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   development: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.USER,
