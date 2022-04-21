8cb300de7979e14580585916db5a78b07c3ea359
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-04-21 17:41:45 +0100

    circleci integration

--- config/config.cjs
+++ config/config.cjs
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
+console.log(process.env.PORT, 'FFFFFFFF', process.env.DATABASE_URL);
 module.exports = {
   development: {
     username: process.env.USER,
@@ -9,6 +10,7 @@ module.exports = {
     host: process.env.HOST,
     dialect: 'postgres',
   },
+
   test: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
