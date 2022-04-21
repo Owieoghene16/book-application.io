1d5c7745734217db3b4cd4127b43ec18449ceec4
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-04-21 16:40:11 +0100

    circleci integration

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import app from './app';
 
-const port = process.env.PORT || 5000;
-
-app.listen(5050, () => {
-  console.log('http://localhost:5000.');
+app.listen(process.env.PORT || 5000, () => {
+  console.log('http://localhost:5000');
 });
