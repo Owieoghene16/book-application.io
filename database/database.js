e19db66f7352dc350b5d61c272eb2f8e4ac7dff6
Owieoghene16 <kindnessokpugie1@gmail.com>
2022-04-25 11:43:28 +0100

    circleci integration

--- database/database.js
+++ database/database.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false,
     },
   },
-  /* host: 'localhost',
+  host: 'localhost',
   dialect: 'postgres',
   operatorsAliases: false,
 
@@ -22,7 +22,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     min: 0,
     acquire: 30000,
     idle: 10000,
-  }, */
+  },
 });
 
 const db = {};
